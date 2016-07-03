8667fd220e8d3b9b1eb39f6a23b86855dec07292
madhukirans <madhukiran.seelam@gmail.com>
2016-07-03 23:06:11 +0530

    new file:   web/app/pages/dashboard/DashboardCtrl.js 	new file:   web/app/pages/dashboard/dashboard.html 	new file:   web/app/pages/dashboard/dashboard.module.js 	new file:   web/app/pages/pagesCtrl.js 	modified:   .gitignore 	modified:   src/java/com/oracle/stagerun/entities/StageUpperstackShiphomesEntity.java 	modified:   src/java/com/oracle/stagerun/entities/service/StageUpperstackShiphomesEntityFacadeREST.java 	modified:   web/a.html 	modified:   web/a.js 	modified:   web/app/pages/admin/addReleases/addReleases.html 	modified:   web/app/pages/admin/addReleases/addReleasesCtrl.js 	modified:   web/app/pages/admin/admin.html 	modified:   web/app/pages/admin/admin.module.js 	modified:   web/app/pages/pages.module.js 	modified:   web/app/pages/shiphomes/shiphomes.module.js 	modified:   web/app/pages/stage/stage.html 	modified:   web/app/pages/stage/stage.module.js 	modified:   web/app/pages/stage/stageRun/StageRunCtrl.js 	modified:   web/app/pages/stage/stageRun/stageRun.directive.js 	modified:   web/app/pages/stage/stageRun/stageRun.html 	modified:   web/app/pages/stage/stageRun/stageRun.module.js 	modified:   web/index.html

---
+++ web/app/pages/dashboard/DashboardCtrl.js
@@ -0,0 +1,14 @@
+/**
+ * @author v.lugovksy
+ * created on 16.12.2015
+ */
+(function () {
+    'use strict';
+
+    angular.module('BlurAdmin.pages.dashboard').controller('DashboardCtrl', DashboardCtrl);
+  
+    function DashboardCtrl($scope, $http) {
+             
+    };
+})();
+
