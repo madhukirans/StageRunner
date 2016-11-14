2ef9b37e648c90af3f652bbff148363c91b3878e
madhukirans <madhukiran.seelam@gmail.com>
2016-11-14 01:42:34 -0800

    modified:   env.sh 	new file:   install_and_configure.doc 	modified:   javasrc/conf/persistence.xml 	renamed:    javasrc/java/META-INF/persistence.xml -> javasrc/java/META-INF/persistence.1.xml 	modified:   lib/nblibraries.properties 	modified:   nbproject/project.properties 	modified:   src/WEB-INF/glassfish-resources.xml 	deleted:    srdemon.sh

--- env.sh
+++ env.sh
@@ -2,7 +2,7 @@
 
 export STAGE_RUNNER_HOME=/scratch/mseelam/sr/StageRunner
 export SERVER_HOME=/scratch/mseelam/glassfish4/glassfish
-export JAVA_HOME=/ade/mseelam_otd5/bootstrap/java/1.8.0-91-14-160401.1.8.0.91.014/jdk1.8.0_91
+export JAVA_HOME=/scratch/mseelam/jdk1.8.0_91
 export PATH=$STAGE_RUNNER_HOME/tools/retool/3.0.0/bin:$PATH
 export BOOTSTRAP_HOME=/scratch/sr/bootstrap
 
